afba6d5ad9bc90acc6f3ce7669ab5a9fba5a988b
Ashcoder232 <aswanths208@gmail.com>
2021-08-17 20:13:57 +0530

    Updating and add javascript session files

---
+++ JS/main2.js
@@ -0,0 +1,16 @@
+let a = 10;
+const pie = 3.14;
+name1 = "Aswanth S";
+name2 = "Ajay Mahan"
+console.log(a);
+console.log(pie);
+display();
+function display() {
+    console.log("Your name is : " + name1);
+
+}
+let show = () => {
+    console.log("Hello " + name2);
+}
+
+show();
