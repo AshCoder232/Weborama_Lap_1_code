11f6ff7a30b480134d4c38f8108230364b757762
Ashcoder232 <aswanths208@gmail.com>
2021-08-20 15:47:55 +0530

    React App Sample

---
+++ counter/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+const App = () =>{
+  
+  let a = 10;
+  return (
+    <div className="App">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <header className="App-header">
+      {a}
+      <h1>Hello World</h1>
+      
+
+        
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
